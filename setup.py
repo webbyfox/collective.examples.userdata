82e5850f6f500f9f84c803bf19fb65509036259a
Jamie Lentin <jm@lentin.co.uk>
2012-11-19 17:37:02 +0000

    It's an rst file, not txt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.4dvl'
 setup(name='collective.examples.userdata',
       version=version,
       description="Showcase for the new (Plone 4) plone.app.users IUserDataSchema. Shows how to extend the user data fields that can be selected for the registration form.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
                        + open(os.path.join("docs", "TODO.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
