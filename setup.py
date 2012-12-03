31337fb5c249e20f092cc841e3f57cf5662fc6c0
Johannes Raggam <raggam-nl@adm.at>
2012-12-03 15:25:04 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.4dvl'
+version = '0.4dev'
 
 setup(name='collective.examples.userdata',
       version=version,
       description="Showcase for the new (Plone 4) plone.app.users IUserDataSchema. Shows how to extend the user data fields that can be selected for the registration form.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read() + "\n"
                        + open(os.path.join("docs", "TODO.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
